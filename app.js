fafb51180932eb3fc6400a5a0e41230e5e52f34d
husna-banu <husnaban24@gmail.com>
2018-11-06 02:37:14 +0530

    file reading

---
+++ app.js
@@ -0,0 +1,10 @@
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function(req,res){
+    console.log('the requested url is'+req.url);
+    fs.createReadStream(__dirname+'/stuff/readMe.txt','utf8').pipe(res);
+});
+
+server.listen(3001,'127.0.0.1');
+console.log('Listening to the port 3001');
